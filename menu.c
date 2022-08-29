4277c950b0db6c2246b0ec5a7e4f3084b83843b5
Bautista Marinelli Scaglia <bautimarinelli100@gmail.com>
2022-08-29 19:09:01 -0300

    First commit

---
+++ menu.c
@@ -0,0 +1,31 @@
+//RAMA MASTER
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main(){
+    
+    int op;
+    
+    
+    printf("Opciones:\n");
+    printf("1.Opcion 1.\n");
+    printf("2.Opcion 2.\n");
+    scanf("%d", &op);
+
+    if (op == 1)
+    {
+        printf("Has ingresado a la opcion 1.\n");
+    }
+    if (op == 2)
+    {
+        printf("Has ingresado a la opcion 2.\n");
+    }
+    system("pause");
+    return 0;
+    
+
+  
+    
+}
